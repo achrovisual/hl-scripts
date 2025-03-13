178c86f8e0b32dae4befda846a9650ddff77f3a8
Eugenio Pastoral <pastoraleugenio@gmail.com>
2025-03-13 12:02:38 +0000

    Add commands to install Argo CD

--- k3s/setup.sh
+++ k3s/setup.sh
@@ -23,11 +23,32 @@ fi
 node_type="$1"
 
 if [[ "$node_type" == "server" ]]; then
+  # Install K3s server
   curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --cluster-init --disable=servicelb
+
+  # Install Helm
+  curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg >/dev/null
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+  sudo apt install update
+  sudo apt install upgrade -y
+
+  sudo apt install helm
+
+  # Add Argo Helm repo
+  sudo helm repo add argo https://argoproj.github.io/argo-helm
+
+  # Install Argo CD with ingress enabled and TLS disabled
+  sudo sudo helm upgrade argo-cd argo/argo-cd --version 7.8.10 --namespace argo-cd --kubeconfig /etc/rancher/k3s/k3s.yaml --create-namespace --set nameOverride=argo-cd --set configs.params.server.insecure=true --set server.ingress.enabled=true
+
+  # Patch argo-cd-server service to LoadBalancer
+  sudo kubectl patch svc argo-cd-server -n argo-cd -p '{"spec": {"type": "LoadBalancer"}}'
+
+  # Print initial admin password
+  sudo kubectl -n argo-cd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 elif [[ "$node_type" == "agent" ]]; then
   curl -sfL https://get.k3s.io | K3S_URL=https://$master_01:6443 K3S_TOKEN=SECRET sh -
 else
   echo "Error: Invalid node type: $node_type."
   exit 1
 fi
-
