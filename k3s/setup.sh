1f1bc60acac03a01d29c8fce9db5eb8d8cf13dbb
Eugenio Pastoral <pastoraleugenio@gmail.com>
2025-03-16 06:03:14 +0000

    Add server token as an argument

--- k3s/setup.sh
+++ k3s/setup.sh
@@ -16,15 +16,19 @@ master_nodes={$master_02 $master_03}
 worker_nodes={$worker_01 $worker_02}
 
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 <node_type>"
+  echo "Usage: $0 <node_type> <secret>"
+  echo "  <node_type>: 'server' or 'agent'"
+  echo "  <server_token>: token for the server"
   exit 1
 fi
 
 node_type="$1"
+server_token="$2"
+
 
 if [[ "$node_type" == "server" ]]; then
   # Install K3s server
-  curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --cluster-init --disable=servicelb
+  curl -sfL https://get.k3s.io | K3S_TOKEN=$server_token sh -s - server --cluster-init --disable=servicelb
 
   # Install Helm
   curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg >/dev/null
@@ -34,12 +38,12 @@ if [[ "$node_type" == "server" ]]; then
   sudo apt install upgrade -y
 
   sudo apt install helm
-  
+
   # Add MetalLB Helm repo
   sudo helm repo add metallb https://metallb.github.io/metallb
-  
+
   # Install MetalLB
-  sudo helm install metallb metallb/metallb --namespace metallb --kubeconfig /etc/rancher/k3s/k3s.yaml --create-namespace 
+  sudo helm install metallb metallb/metallb --namespace metallb --kubeconfig /etc/rancher/k3s/k3s.yaml --create-namespace
 
   # Add Argo Helm repo
   sudo helm repo add argo https://argoproj.github.io/argo-helm
@@ -50,7 +54,7 @@ if [[ "$node_type" == "server" ]]; then
   # Print initial admin password
   sudo kubectl -n argo-cd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 elif [[ "$node_type" == "agent" ]]; then
-  curl -sfL https://get.k3s.io | K3S_URL=https://$master_01:6443 K3S_TOKEN=SECRET sh -
+  curl -sfL https://get.k3s.io | K3S_URL=https://$master_01:6443 K3S_TOKEN=$server_token sh -
 else
   echo "Error: Invalid node type: $node_type."
   exit 1
